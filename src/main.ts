8a81e74f4d3a4370166f315627e0d7fdbfd46528
Silich <sili41995@gmail.com>
2025-10-02 11:54:37 +0300

    add README file

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
-  const port = configService.get<string>('PORT') || 3000;
-  const frontendUrl = configService.get<string>('FRONTEND_URL') || 3000;
+  const port = configService.get<string>('PORT') || 4000;
+  const frontendUrl = configService.get<string>('FRONTEND_URL');
 
   app.setGlobalPrefix('api');
   app.enableCors({
